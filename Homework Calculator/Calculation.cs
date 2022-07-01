97a5702fd9c609cd1d012940fc23d0afb1320d2f
hagverdi11 <hagverdiam@code.edu.az>
2022-07-01 04:54:54 +0400

    Added homework table

---
+++ Homework Calculator/Calculation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_Calculator
+{
+    public class Calculation : ICalculation
+    {
+      
+        public void Calculate(int num1, string Operation, int num2)
+        {
+            if (Operation == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+
+            if (Operation == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+
+            if (Operation == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+
+            if (Operation == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+
+          
+        }
+    }
+
+
+
+    public interface ICalculation
+    {
+        void Calculate(int num1, string Operation, int num2);
+    }
+}
---
+++ Homework Calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Homework_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation calculation = new Calculation();
+
+
+
+            //Type your digits 
+
+
+            calculation.Calculate(16, "/", 8);
+            calculation.Calculate(16, "*", 8);
+            calculation.Calculate(16, "-", 8);
+            calculation.Calculate(16, "+", 8);
+
+        }
+    }
+}
